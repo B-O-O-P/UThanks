25151e25daf477cc4df3754ddea745cf24c1355d
valentinnodan <valentinka-danilova@mail.ru>
2020-03-11 00:51:43 +0300

    fixed opacity and listener

--- src/main/resources/static/js/arrow-to-top.js
+++ src/main/resources/static/js/arrow-to-top.js
@@ -2,16 +2,17 @@ const arrow = document.getElementsByClassName('arrow-to-top')[0];
 
 arrow.style.display = 'none';
 
-arrow.onclick = function () {
+arrow.addEventListener('click', function () {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-};
+});
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         arrow.style.display = "block";
+        arrow.style.opacity = '0.6';
     } else {
         arrow.style.display = "none";
     }
