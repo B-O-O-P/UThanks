2a2e6cc09b067feb7bf7aaa0d2decb7ee47b6561
polinb <PolinB2413@gmail.com>
2020-03-11 03:23:07 +0300

    Deleted not used imports

--- src/test/java/com/uthanks/RegisterValidationTest.java
+++ src/test/java/com/uthanks/RegisterValidationTest.java
@@ -1,16 +1,9 @@
 package com.uthanks;
 
-import com.uthanks.controller.RegisterPage;
-import com.uthanks.domain.User;
 import com.uthanks.form.UserCredentials;
 import com.uthanks.form.validators.UserCredentialsRegisterValidator;
-import com.uthanks.repository.UserRepository;
-import com.uthanks.services.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,10 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
-
-import static org.junit.Assert.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 // Used our database(
 @RunWith(SpringRunner.class)
