d3c3f18149b30258b7819a0c66fd3b7ec62b2f71
Chizhikov Dmitriy <dimkanut@gmail.com>
2020-03-13 00:25:53 +0300

    optimize imports

--- src/main/java/com/uthanks/domain/User.java
+++ src/main/java/com/uthanks/domain/User.java
@@ -1,10 +1,26 @@
 package com.uthanks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.UniqueConstraint;
+import javax.persistence.JoinColumn;
+import javax.persistence.FetchType;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OrderBy;
+import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinTable;
+import javax.persistence.CascadeType;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
