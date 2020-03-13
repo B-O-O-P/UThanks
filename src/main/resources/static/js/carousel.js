cb7e59af5d362c580f2050e6cbe6efcb5d8442fb
valentinnodan <valentinka-danilova@mail.ru>
2020-03-13 17:04:44 +0300

    Fixed arrows

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -6,45 +6,19 @@ let count = 0;
 let widthCard = 200;
 let margins = 0;
 
-function resizing() {
-    wrapper.style.display = 'none';
-
-    let widthSuper = parseInt(carouselSuperWrapper.offsetWidth);
-    count = (widthSuper - (widthSuper % widthCard)) / widthCard;
-    margins = ((widthSuper % widthCard)) / (Math.min(count, cards.length) + 1);
-
-    if (Math.min(count, cards.length) === cards.length) {
-        margins += ((widthCard) * (count - cards.length)) / (cards.length + 1);
-    }
-
-    carouselSuperWrapper.style.width = `${carouselSuperWrapper.offsetWidth}px`;
-
-    cards.forEach(card => {
-        card.style.margin = `0 ${margins / 2}px`;
-    });
-    if (cards.length !== 0) {
-        cards[0].style.marginLeft =  `${margins}px`;
-        cards[cards.length - 1].style.marginRight = `${margins}px`
-    }
-
-    wrapper.style.display = 'flex';
-}
-
-resizing();
-
-let cardsInLeft = 0; // положение ленты прокрутки
-
 const arrowLeft = document.getElementsByClassName('event-carousel__arrow_left')[0];
 const arrowRight = document.getElementsByClassName('event-carousel__arrow_right')[0];
+let cardsInLeft = 0;
+
 
 function toRight() {
-    if (cardsInLeft !== Math.max(cards.length - count, 0)) {
+    if (cardsInLeft < Math.max(cards.length - count, 0)) {
         cardsInLeft++;
         let pxToTransform = cardsInLeft * (margins + widthCard);
         wrapper.style.transform = `translateX(${pxToTransform * (-1)}px)`;
         arrowLeft.style.opacity = '1';
     }
-    if (cardsInLeft === Math.max(cards.length - count, 0)) {
+    if (cardsInLeft >= Math.max(cards.length - count, 0)) {
         arrowRight.style.opacity = '0.5'
     }
 }
@@ -55,6 +29,9 @@ function toLeft() {
         let pxToTransform = cardsInLeft * (margins + widthCard);
         wrapper.style.transform = `translateX(${pxToTransform * (-1)}px)`;
         arrowRight.style.opacity = '1';
+        if (cardsInLeft >= Math.max(cards.length - count, 0)) {
+            arrowRight.style.opacity = '0.5'
+        }
     }
     if (cardsInLeft === 0) {
         arrowLeft.style.opacity = '0.5'
@@ -98,6 +75,40 @@ function throttle(func, ms) {
 
     return wrapper;
 }
+
+function resizing() {
+    arrowRight.style.opacity = '1';
+    arrowLeft.style.opacity = '1';
+    wrapper.style.display = 'none';
+
+    let widthSuper = parseInt(carouselSuperWrapper.offsetWidth);
+    count = (widthSuper - (widthSuper % widthCard)) / widthCard;
+    margins = ((widthSuper % widthCard)) / (Math.min(count, cards.length) + 1);
+
+    if (Math.min(count, cards.length) === cards.length) {
+        margins += ((widthCard) * (count - cards.length)) / (cards.length + 1);
+    }
+
+    carouselSuperWrapper.style.width = `${carouselSuperWrapper.offsetWidth}px`;
+
+    cards.forEach(card => {
+        card.style.margin = `0 ${margins / 2}px`;
+    });
+    if (cards.length !== 0) {
+        cards[0].style.marginLeft =  `${margins}px`;
+        cards[cards.length - 1].style.marginRight = `${margins}px`
+    }
+
+    wrapper.style.display = 'flex';
+    if (cardsInLeft === 0){
+        arrowLeft.style.opacity = '0.5';
+    }
+    if (cardsInLeft >= Math.max(cards.length - count, 0)) {
+        arrowRight.style.opacity = '0.5'
+    }
+}
+
+resizing();
 
 window.addEventListener('resize', throttle(function () {
     carouselSuperWrapper.style.width = '100%';
