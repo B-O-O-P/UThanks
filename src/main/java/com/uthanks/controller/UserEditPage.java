75866a9af44bd550c4f196e184abd9ae16cd6d1d
polinb <PolinB2413@gmail.com>
2020-03-13 23:45:48 +0300

    Deleted UserTest, added validator for edit user, added test for edit user validator

--- src/main/java/com/uthanks/controller/UserEditPage.java
+++ src/main/java/com/uthanks/controller/UserEditPage.java
@@ -1,6 +1,8 @@
 package com.uthanks.controller;
 
 import com.uthanks.domain.User;
+import com.uthanks.form.UserAdditionalInfo;
+import com.uthanks.form.validators.UserAdditionalInfoValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,12 +14,28 @@ import javax.validation.Valid;
 
 @Controller
 public class UserEditPage extends Page {
+    private final UserAdditionalInfoValidator userAdditionalInfoValidator;
+
+    public UserEditPage(UserAdditionalInfoValidator userAdditionalInfoValidator) {
+        this.userAdditionalInfoValidator = userAdditionalInfoValidator;
+    }
+
+    @InitBinder("userInfo")
+    public void initRegisterFormBinder(WebDataBinder binder) {
+        binder.addValidators(userAdditionalInfoValidator);
+    }
+
     @GetMapping(path = "/user/{id}/edit")
-    public String editGet(@PathVariable("id") String requestId, Model model) {
+    public String editGet(@PathVariable("id") String requestId, Model model, HttpSession httpSession) {
         try {
+            User sessionUser = getUser(httpSession);
+            Long requestNumberId = Long.parseLong(requestId);
+            if (sessionUser == null || requestNumberId != sessionUser.getId()) {
+                return "redirect:/not-found";
+            }
             User user = getUserService().findById(Long.parseLong(requestId));
             if (user != null) {
-                model.addAttribute("userInfo", new User());
+                model.addAttribute("userInfo", new UserAdditionalInfo());
             }
         } catch (NumberFormatException e) {
             return "redirect:/not-found";
@@ -26,15 +44,16 @@ public class UserEditPage extends Page {
     }
 
     @PostMapping(path = "/user/{id}/edit")
-    public String editPost(@Valid @ModelAttribute("userInfo") User updatedUser,
+    public String editPost(@Valid @ModelAttribute("userInfo") UserAdditionalInfo additionalInfo,
                            BindingResult bindingResult,
                            HttpSession httpSession, @PathVariable("id") String postId) {
-
-        // TODO create addedInfoClass and validate it
+        if (bindingResult.hasErrors()) {
+            return "user-edit";
+        }
 
         try {
-            setUser(httpSession, getUserService().saveAdditionalInfo(Long.parseLong(postId), updatedUser.getAge(),
-                    updatedUser.getCountry(), updatedUser.getName(), updatedUser.getSkills()));
+            setUser(httpSession, getUserService().saveAdditionalInfo(Long.parseLong(postId), additionalInfo.getAge(),
+                    additionalInfo.getCountry(), additionalInfo.getName(), additionalInfo.getSkills()));
         } catch (NumberFormatException e) {
             return "user-edit";
         }
