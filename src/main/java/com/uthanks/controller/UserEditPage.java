fe66f42c6b89916ad42b8ad2a32a6699fa55d4ce
polinb <PolinB2413@gmail.com>
2020-03-13 17:19:19 +0300

    Deleted userEdit Validator

--- src/main/java/com/uthanks/controller/UserEditPage.java
+++ src/main/java/com/uthanks/controller/UserEditPage.java
@@ -1,7 +1,6 @@
 package com.uthanks.controller;
 
 import com.uthanks.domain.User;
-import com.uthanks.form.validators.EditUserValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,23 +12,12 @@ import javax.validation.Valid;
 
 @Controller
 public class UserEditPage extends Page {
-    private final EditUserValidator editUserValidator;
-
-    public UserEditPage(EditUserValidator editUserValidator) {
-        this.editUserValidator = editUserValidator;
-    }
-
-    @InitBinder("userInfo")
-    public void initRegisterFormBinder(WebDataBinder binder) {
-        binder.addValidators(editUserValidator);
-    }
-
     @GetMapping(path = "/user/{id}/edit")
     public String editGet(@PathVariable("id") String requestId, Model model) {
         try {
             User user = getUserService().findById(Long.parseLong(requestId));
             if (user != null) {
-                model.addAttribute("userInfo", user);
+                model.addAttribute("userInfo", new User());
             }
         } catch (NumberFormatException e) {
             return "redirect:/not-found";
@@ -41,9 +29,8 @@ public class UserEditPage extends Page {
     public String editPost(@Valid @ModelAttribute("userInfo") User updatedUser,
                            BindingResult bindingResult,
                            HttpSession httpSession, @PathVariable("id") String postId) {
-        if (bindingResult.hasErrors()) {
-            return "user-edit";
-        }
+
+        // TODO create addedInfoClass and validate it
 
         try {
             setUser(httpSession, getUserService().saveAdditionalInfo(Long.parseLong(postId), updatedUser.getAge(),
